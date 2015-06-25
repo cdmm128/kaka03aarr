a569998ec842157c0e53bde265ce36e4013bf7ba
cdmm128 <tubabassclark@gmail.com>
2015-06-25 13:12:54 +0800

    test321564987

--- MainActivity.java
+++ MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+//clakrk 0123456789
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
